720ba7e3d8eef914253aca26503be29ba60cdbd7
anghernandez <kari345hernandez@gmail.com>
2026-06-23 09:40:14 -0600

    feat: implement single player pong in C

---
+++ pong_game/src/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SCREEN_WIDTH   640
+#define SCREEN_HEIGHT  480
+
+#define PADDLE_WIDTH   10
+#define PADDLE_HEIGHT  60
+#define PADDLE_SPEED   4
+
+#define BALL_SIZE      8
+#define BALL_SPEED_X   3
+#define BALL_SPEED_Y   2
+
+#define CPU_SPEED      3
+
+#endif
